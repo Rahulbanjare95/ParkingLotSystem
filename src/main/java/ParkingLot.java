1cd2b199acc15fa3ed5435e3614cbd4a25ee950c
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-22 14:39:43 +0530

    Refactored: Code to allow multiple vehicles to be parked

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,36 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
 
 public class ParkingLot {
+    private int originalCapacity;
     public Object vehicle;
     private Owner owner;
+    private int currentCapacity;
 
-    public ParkingLot() {
+    List vehicles;
+    public ParkingLot(int capacity) {
+        this.originalCapacity = capacity;
+        this.vehicles = new ArrayList();
     }
 
     public void park(Object vehicle) throws ParkingLotException {
-        if( this.vehicle != null )
-            throw  new ParkingLotException("Parking Full",
+        if( this.vehicles.size() > this.originalCapacity ) {
+            owner.parkingLotFull();
+            throw new ParkingLotException("Parking Full",
                     ParkingLotException.ExceptionType.FULL);
-        this.vehicle = vehicle;
+        }
+        if (this.isVehicleParked(vehicle))
+            throw new ParkingLotException("Vehicle Already Parked", ParkingLotException.ExceptionType.ALREADY_PARKED);
+        this.vehicles.add(vehicle);
     }
     public boolean isVehicleParked(Object vehicle) {
-        if(this.vehicle.equals(vehicle))
+        if(this.vehicles.contains(vehicle))
             return true;
         return false;
     }
 
     public boolean unPark(Object vehicle) throws ParkingLotException {
         if(this.vehicle == null) return  false;
-        if (this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
+        if (this.vehicles.contains(vehicle)) {
+            this.vehicles.remove(vehicle);
             return true;
         }
         throw new ParkingLotException("No such model exist", ParkingLotException.ExceptionType.WRONG_DETAILS);
     }
 
-
-    public boolean registerOwner(Owner owner) {
+    public void registerOwner(Owner owner) {
         this.owner = owner;
-        return  true;
     }
 
+    public void setCapacity(int capacity) {
+        this.originalCapacity = capacity;
+    }
 }
